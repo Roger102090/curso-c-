5fd90de5b5f35f1ecb9f0df0391bf0374109372f
Roger102090 <130093686+Roger102090@users.noreply.github.com>
2023-04-11 00:28:49 +0000

    curso c++

---
+++ TiposDeDatosBasicos.cpp
@@ -0,0 +1,15 @@
+//tipos de datos basicos
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int entero = 15;
+    float flotante = 44.5;
+    double doble = 4.45612346;
+    char letra = 'a';
+    cout<<letra;
+
+    return 0;
+}
---
+++ curso.cpp
@@ -0,0 +1,13 @@
+//primer programa en c++
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"hola mundo"<<endl;
+    // cout<<"hola mundo/n";
+    /* code */
+    return 0;
+}

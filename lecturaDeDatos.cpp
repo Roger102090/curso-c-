4138b7a6c4aaba8d387dfc495f72348e5c1a1bea
Roger102090 <130093686+Roger102090@users.noreply.github.com>
2023-04-11 01:06:08 +0000

    checkpoint

---
+++ lecturaDeDatos.cpp
@@ -0,0 +1,18 @@
+//Lectura de datos
+#include<iostream>
+
+using namespace std;
+
+/* aqui adentro es comentario*/
+
+
+int main() {
+    float numero;//definiendo la variable
+    
+    cout<<"digite el numero: ";
+    cin>>numero;//guardando la variable
+
+    cout<<"\nEl numero que digito es: "<<numero;
+
+    return 0;
+}
